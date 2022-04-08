078d284d8d435b2919fa77c66fde34d695a04ae8
Hong Linh <71358321+lelinh014756@users.noreply.github.com>
2022-04-08 10:13:30 +0700

    update animation goToTop

--- src/common/GoToTop/GoToTop.jsx
+++ src/common/GoToTop/GoToTop.jsx
@@ -8,7 +8,7 @@ const GoToTop = () => {
 
   return (
     <div
-      className="fixed sm:hidden bottom-[4rem] right-[4rem] text-[3rem] text-[#171717] p-[1rem] rounded-full bg-[#dbdbd8] cursor-pointer hover:text-primary"
+      className="fixed sm:hidden bottom-[4rem] right-[4rem] text-[3rem] text-[#171717] p-[1rem] rounded-full bg-[#dbdbd8] cursor-pointer hover:text-primary animate-[opacity-show_0.3s_linear]"
       onClick={HandleScroll}
     >
       <IoIosArrowUp />
